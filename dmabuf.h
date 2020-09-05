d7648278c9d2f553495151b10d0fb01dc524dcdb
Alexandr Kozlinskiy <akozlins@gmail.com>
2020-09-05 17:02:06 +0200

    add dmabuf_entry_for_each, use __GFP_NOWARN

--- dmabuf.h
+++ dmabuf.h
@@ -9,6 +9,9 @@ struct dmabuf_entry {
     dma_addr_t dma_handle;
 };
 
+#define dmabuf_entry_for_each(pos, head) \
+    for(struct dmabuf_entry* pos = &(*head)[0]; pos->size != 0; pos++)
+
 struct dmabuf {
     struct device* dev;
     size_t size;
@@ -18,17 +21,12 @@ struct dmabuf {
 
 static
 void dmabuf_free(struct dmabuf* dmabuf) {
-    struct dmabuf_entry* entry;
-
     M_INFO("\n");
 
     if(IS_ERR_OR_NULL(dmabuf)) return;
 
-    for(int i = 0; i < dmabuf->count; i++) {
-        entry = &dmabuf->entries[i];
-        if(entry->size == 0) continue;
-
-        M_DEBUG("dma_free_coherent(dma_handle = 0x%llx))\n", entry->dma_handle);
+    dmabuf_entry_for_each(entry, &dmabuf->entries) {
+        M_DEBUG("dma_free_coherent(dma_handle = 0x%llx, size = 0x%lx)\n", entry->dma_handle, entry->size);
         dma_free_coherent(dmabuf->dev, entry->size, entry->cpu_addr, entry->dma_handle);
     }
 
@@ -38,16 +36,21 @@ void dmabuf_free(struct dmabuf* dmabuf) {
 static
 struct dmabuf* dmabuf_alloc(struct device* dev, size_t size) {
     int error;
-    struct dmabuf* dmabuf;
-    int count = ALIGN(size, PAGE_SIZE << 10) / (PAGE_SIZE << 10);
+    size_t entry_size = PAGE_SIZE << 10;
+    int count = ALIGN(size, entry_size) / entry_size;
+    struct dmabuf* dmabuf = NULL;
 
     M_INFO("size = 0x%lx\n", size);
+
+    if(dev == NULL || size == 0 || !IS_ALIGNED(size, PAGE_SIZE)) {
+        error = -EINVAL;
+        goto err_out;
+    }
 
     dmabuf = kzalloc(sizeof(*dmabuf) + count * sizeof(dmabuf->entries[0]), GFP_KERNEL);
     if(IS_ERR_OR_NULL(dmabuf)) {
         error = PTR_ERR(dmabuf);
         if(error == 0) error = -ENOMEM;
-        dmabuf = NULL;
         M_ERR("kzalloc: error = %d\n", error);
         goto err_out;
     }
@@ -57,15 +60,21 @@ struct dmabuf* dmabuf_alloc(struct device* dev, size_t size) {
 
     for(int i = 0; i < dmabuf->count; i++) {
         struct dmabuf_entry* entry = &dmabuf->entries[i];
+        if(IS_ERR_OR_NULL(entry)) {
+            error = PTR_ERR(entry);
+            if(error == 0) error = -ENOMEM;
+            M_ERR("kzalloc: error = %d\n", error);
+            goto err_out;
+        }
 
-        entry->size = PAGE_SIZE << 10;
 
+        entry->size = entry_size;
         M_DEBUG("dma_alloc_coherent(size = 0x%lx)\n", entry->size);
-        entry->cpu_addr = dma_alloc_coherent(dmabuf->dev, entry->size, &entry->dma_handle, GFP_ATOMIC); // see `pci_alloc_consistent`
+        entry->cpu_addr = dma_alloc_coherent(dmabuf->dev, entry->size, &entry->dma_handle, GFP_ATOMIC | __GFP_NOWARN); // see `pci_alloc_consistent`
         if(IS_ERR_OR_NULL(entry->cpu_addr)) {
             error = PTR_ERR(entry->cpu_addr);
             if(error == 0) error = -ENOMEM;
-            M_ERR("dma_alloc_coherent: error = %d\n", error);
+            M_ERR("dma_alloc_coherent(size = 0x%lx): error = %d\n", entry->size, error);
             entry->size = 0;
             goto err_out;
         }
@@ -124,8 +133,7 @@ int dmabuf_mmap(struct dmabuf* dmabuf, struct vm_area_struct* vma) {
     // see `https://www.kernel.org/doc/html/latest/x86/pat.html#advanced-apis-for-drivers`
     vma->vm_page_prot = pgprot_noncached(vma->vm_page_prot); // see `pgprot_dmacoherent`
 
-    for(int i = 0; i < dmabuf->count; i++) {
-        struct dmabuf_entry* entry = &dmabuf->entries[i];
+    dmabuf_entry_for_each(entry, &dmabuf->entries) {
         unsigned long pfn = PHYS_PFN(dma_to_phys(dmabuf->dev, entry->dma_handle)); // see `dma_direct_mmap`
 
         M_DEBUG("remap_pfn_range(pfn = 0x%lx, size = 0x%lx)\n", pfn, entry->size);
@@ -135,7 +143,7 @@ int dmabuf_mmap(struct dmabuf* dmabuf, struct vm_area_struct* vma) {
             entry->size, vma->vm_page_prot
         );
         if(error) {
-            M_ERR("remap_pfn_range: error = %d\n", error);
+            M_ERR("remap_pfn_range(pfn = 0x%lx, size = 0x%lx): error = %d\n", pfn, entry->size, error);
             break;
         }
 
@@ -146,35 +154,33 @@ int dmabuf_mmap(struct dmabuf* dmabuf, struct vm_area_struct* vma) {
 }
 
 static
-ssize_t dmabuf_read(struct dmabuf* dmabuf, char __user* user_buffer, size_t size, loff_t offset) {
+ssize_t dmabuf_read(struct dmabuf* dmabuf, char __user* user_buffer, size_t user_size, loff_t offset) {
     ssize_t n = 0;
-    struct dmabuf_entry* entry;
 
     if(dmabuf == NULL) {
         M_ERR("dmabuf == NULL\n");
         return -EFAULT;
     }
 
-    for(int i = 0; i < dmabuf->count; i++) {
-        size_t k;
-        entry = &dmabuf->entries[i];
-        if(size == 0) break;
+    dmabuf_entry_for_each(entry, &dmabuf->entries) {
+        size_t size;
+        if(user_size == 0) break;
         if(offset >= entry->size) {
             offset -= entry->size;
             continue;
         }
 
-        k = entry->size - offset;
-        if(k > size) k = size;
+        size = entry->size - offset;
+        if(size > user_size) size = user_size;
 
-        M_DEBUG("copy_to_user(size = 0x%lx)\n", k);
-        if(copy_to_user(user_buffer, entry->cpu_addr + offset, k)) {
-            M_ERR("copy_to_user != 0\n");
+        M_DEBUG("copy_to_user(size = 0x%lx)\n", size);
+        if(copy_to_user(user_buffer, entry->cpu_addr + offset, size)) {
+            M_ERR("copy_to_user(size = 0x%lx) != 0\n", size);
             return -EFAULT;
         }
-        n += k;
-        user_buffer += k;
-        size -= k;
+        n += size;
+        user_buffer += size;
+        user_size -= size;
         offset = 0;
     }
 
@@ -182,35 +188,33 @@ ssize_t dmabuf_read(struct dmabuf* dmabuf, char __user* user_buffer, size_t size
 }
 
 static
-ssize_t dmabuf_write(struct dmabuf* dmabuf, const char __user* user_buffer, size_t size, loff_t offset) {
+ssize_t dmabuf_write(struct dmabuf* dmabuf, const char __user* user_buffer, size_t user_size, loff_t offset) {
     ssize_t n = 0;
-    struct dmabuf_entry* entry;
 
     if(dmabuf == NULL) {
         M_ERR("dmabuf == NULL\n");
         return -EFAULT;
     }
 
-    for(int i = 0; i < dmabuf->count; i++) {
-        size_t k;
-        entry = &dmabuf->entries[i];
-        if(size == 0) break;
+    dmabuf_entry_for_each(entry, &dmabuf->entries) {
+        size_t size;
+        if(user_size == 0) break;
         if(offset >= entry->size) {
             offset -= entry->size;
             continue;
         }
 
-        k = entry->size - offset;
-        if(k > size) k = size;
+        size = entry->size - offset;
+        if(size > user_size) size = user_size;
 
-        M_DEBUG("copy_from_user(size = 0x%lx)\n", k);
-        if(copy_from_user(entry->cpu_addr + offset, user_buffer, k)) {
-            M_ERR("copy_from_user != 0\n");
+        M_DEBUG("copy_from_user(size = 0x%lx)\n", size);
+        if(copy_from_user(entry->cpu_addr + offset, user_buffer, size)) {
+            M_ERR("copy_from_user(size = 0x%lx) != 0\n", size);
             return -EFAULT;
         }
-        n += k;
-        user_buffer += k;
-        size -= k;
+        n += size;
+        user_buffer += size;
+        user_size -= size;
         offset = 0;
     }
 
