5f5cebe431f4e77edb1f74528bc4cf59c2f2ee31
akozlins <>
2020-09-06 00:07:49 +0200

    use pr_debug for M_DEBUG define

--- kmodule.h
+++ kmodule.h
@@ -4,10 +4,10 @@
 #include <linux/printk.h>
 
 #define M_ERR(fmt, ...) \
-    printk(KERN_ERR   "[%s/%s] " pr_fmt(fmt), THIS_MODULE->name, __FUNCTION__, ##__VA_ARGS__)
+    pr_err("[%s/%s] " pr_fmt(fmt), THIS_MODULE->name, __FUNCTION__, ##__VA_ARGS__)
 #define M_INFO(fmt, ...) \
-    printk(KERN_INFO  "[%s/%s] " pr_fmt(fmt), THIS_MODULE->name, __FUNCTION__, ##__VA_ARGS__)
+    pr_info("[%s/%s] " pr_fmt(fmt), THIS_MODULE->name, __FUNCTION__, ##__VA_ARGS__)
 #define M_DEBUG(fmt, ...) \
-    printk(KERN_DEBUG "[%s/%s] " pr_fmt(fmt), THIS_MODULE->name, __FUNCTION__, ##__VA_ARGS__)
+    pr_debug("[%s/%s] " pr_fmt(fmt), THIS_MODULE->name, __FUNCTION__, ##__VA_ARGS__)
 
 #endif // __KMODULE_H__
